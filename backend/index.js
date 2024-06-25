4e8f21bae795c37fc884911194567eb067fa4926
root <root@gitlab.cujae.edu.cu>
2024-06-25 16:27:30 -0400

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
 let notes = [
   { id: 1, content: "HTML is easy", important: true },
   { id: 2, content: "Browser can execute only JavaScript", important: false },
@@ -21,10 +22,44 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get('/api/notes/:id',(request,response)=>{
-    const id = Number(request.params.id);
-    const note = notes.find((note) => note.id === id)
-    return response.json(note)
-})
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id)
+  
+  if(note){
+    response.json(note)
+    
+  }else{
+    response.status(404).end()
+    }
+  })
+  
 
-const PORT = 3001;
+
+  const generateId=()=>{
+    const maxId = notes.length > 0 ? Math.max(...notes.map( n => n.id)) : 0 
+    return maxId + 1
+  }
+
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    
+    response.status(204).end()
+  })
+  app.post('/api/notes', (request, response) => { 
+    const body  = request.body
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+    const note = request.body 
+    console.log(note)  
+    response.json(note)
+  })
+  
+  
+  
+  const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
